be3c4e8ea719dcf5b5c44bebc14f1fc0f3a72e93
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-06-01 09:57:28 +0200

    tt

---
+++ Ratp/Models/RatpAttachement.swift
@@ -0,0 +1,15 @@
+//
+//  RatpAttachement.swift
+//  Ratp
+//
+//  Created by Ilyess NAIT BELKACEM on 01/06/2022.
+//
+
+import Foundation
+
+class RatpAttachement {
+    var id: String
+    var mimetype: String
+    var title: String
+    var url: String
+}
---
+++ Ratp/Models/RatpDataset.swift
@@ -0,0 +1,16 @@
+//
+//  RatpDataset.swift
+//  Ratp
+//
+//  Created by Ilyess NAIT BELKACEM on 01/06/2022.
+//
+
+import Foundation
+
+class RatpDataset {
+    var id: String;
+    var uid: String;
+    var hasRecords: Bool
+    var feature: [String]
+    var visibility: [RatpAttachement]
+}
